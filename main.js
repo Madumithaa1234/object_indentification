8e136d682e1c83c3c16e2d82c5f213f4a6750c2a
Madumithaa1234 <89779017+Madumithaa1234@users.noreply.github.com>
2022-11-18 18:43:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 img = "";
+status = "";
+object = [];
 
-function setUp(){
+function setup(){
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status : detecting objects";
 }
 
 function preload(){
@@ -11,5 +15,27 @@ function preload(){
 
 function draw(){
     image(img, 0, 0, 640, 420);
+    fill("#07fa34");
+    text("dog", 98, 70);
+    noFill();
+    stroke("#07fa34");
+    rect(99, 40, 200, 200 );
+    fill("#07fa34");
+    text("cat", 320, 70);
+    noFill();
+    stroke("#07fa34");
+    rect(300, 40, 400, 300 );
+}
 
+function modelLoaded(){
+    console.log("modelLoaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
 }
